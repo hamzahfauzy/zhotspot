c003ffc465d30a256e7ba1b6c699e076a882491b
Hamzah Fauzi <muhammadf@thebigrichgroup.com>
2019-11-05 17:08:00 +0700

    instalation to installation

--- resources/views/user/device/error.blade.php
+++ resources/views/user/device/error.blade.php
@@ -8,7 +8,7 @@
         		<h2>Error. RouterOS not Connected</h2>
 			      <div class="alert alert-danger alert-block">
 			        <button type="button" class="close" data-dismiss="alert">×</button> 
-			        <strong>Your router is not connected to this application. Please check the instalation instruction.</strong>
+			        <strong>Your router is not connected to this application. Please check the installation instruction.</strong>
 			      </div>
 	        </div>
         </div>
